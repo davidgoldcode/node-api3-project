ee2889ce8df642ff6c712d58acc73d346f69ddb9
David Gold <davidgold@hey.com>
2020-09-30 16:36:20 -0400

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+const log = morgan("combined");
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.headers);
+  next();
+}
+
+server.use(express.json());
+server.use(log);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
