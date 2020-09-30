c01f9866e6f7fc214f0f3b66ee8c23127f7694d6
David Gold <davidgold@hey.com>
2020-09-30 18:47:37 -0400

    almost done w userrouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("./userDb.js");
 const Post = require("../posts/postDb");
 const { getById } = require("./userDb.js");
+const { json } = require("express");
 const router = express.Router();
 
 // not fully working
@@ -57,11 +58,39 @@ router.get("/:id", (req, res) => {
 });
 
 router.get("/:id/posts", (req, res) => {
-  // do your magic!
+  const user_id = req.params.id;
+  console.log(user_id);
+  User.getUserPosts(user_id)
+    .then((posts) => {
+      if (user_id) {
+        res.status(200).json({ posts });
+      } else {
+        json.status(404).json({ message: "Error!" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      json.status(500).json({ message: "Could not process!" });
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  User.remove(id)
+    .then((user) => {
+      if (id) {
+        res.status(200).json({ user });
+      } else {
+        res.status(404).json({ message: "Unable to delete this user " });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "Unable to perform this action at this time" });
+    });
 });
 
 router.put("/:id", (req, res) => {
