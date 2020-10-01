0c4bc5f008e0746d59919b5123795cd7749d9276
David Gold <davidgold@hey.com>
2020-10-01 18:55:50 -0400

    pushing...

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const postRouter = require("./posts/postRouter");
 const server = express();
 
 //custom middleware
-const users = require("./data/seeds/02-users.js");
 const logger = morgan("combined"); // global
 
 server.use(express.json());
 server.use(logger);
 
 server.get("/", (req, res) => {
-  res.status(200).json({ users });
+  res.status(200).json({ message: "Hello" });
 });
 
 server.use("/api/posts", postRouter);
