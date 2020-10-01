b3b634c23052e5c205b957e514fab0460028561f
David Gold <davidgold@hey.com>
2020-10-01 18:32:20 -0400

    shifted users to be returned

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const postRouter = require("./posts/postRouter");
 const server = express();
 
 //custom middleware
+const users = require("./data/seeds/02-users.js");
 const logger = morgan("combined"); // global
 
 server.use(express.json());
 server.use(logger);
 
 server.get("/", (req, res) => {
-  res.status(200).json({ message: "It's working! " });
+  res.status(200).json({ users });
 });
 
 server.use("/api/posts", postRouter);
